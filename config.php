304bbbe36ad6241ee8b82c72a7a22cf6c7ebafb4
Bruno Felipe <bruno.felipe.ney@gmail.com>
2020-04-19 13:08:30 -0300

    enviando arquivos

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register(function($class_name){
+
+	$filename = $class_name.".php";//variavel para classe $class_name
+
+	if (file_exists($filename/*chamando apenas a variavel da $class_nam,e*/)) {
+		require_once($filename);
+	}
+});
+
+
+?>
